7841212f4065aaee8e5875cae29388ad62a206bf
Ethan Bordeaux <ethanbordeaux@Ethan-Bordeauxs-MacBook-Pro.local>
2015-03-14 15:17:18 -0700

    Initial Commit

---
+++ SpriteWalkthrough/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SpriteWalkthrough
+//
+//  Created by Ethan Bordeaux on 3/14/15.
+//  Copyright (c) 2015 Ethan Bordeaux. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
